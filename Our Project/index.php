ad1550f646e6531d0395187705b498d8d95040ad
jannat2rahul <rahulkumar.rkv@gmail.com>
2016-05-25 19:00:46 +0530

    Add files via upload

---
+++ Our Project/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<meta name="author" content="Rahul Kumar"/>
+		<title>Shake Ur Hand</title>
+		<link rel="icon" type="image/png" href="images/favicon.png"/>
+		
+		<!--adding bootstrap css-->
+		<link rel="stylesheet" href="css/bootstrap-theme.min.css"/>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		
+		<!--adding defined stylesheet files-->
+		<link rel="stylesheet" href="css/style.css"/>
+		<link rel="stylesheet" href="css/nav.css"/>
+		
+	</head>
+	
+	<body id="myPage" class="container">
+	
+		<div class="bar">
+			<form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+				  <input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			
+			<a href="#"  style="color:white;"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+			<a href="#" style="color:white;"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+		</div>
+
+		<!--start of nav bar-->
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>                        
+			  </button>
+			  <a class="navbar-brand" href="#">Logo</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav navbar-right">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#">Mobiles & Electronics<span class="caret"></span></a></li>
+				<li><a href="#">Deals<span class="caret"></span></a></li>
+				<li><a href="#">Community<span class="caret"></span></a></li>
+				<li><a href="#">Fashion<span class="caret"></span></a></li>
+				<li><a href="#">Books<span class="caret"></span></a></li>
+				<li><a href="#">Home Store<span class="caret"></span></a></li>
+				<li><a href="#">Appliance<span class="caret"></span></a></li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+		<!--end of nav bar-->
+		
+		<!--start of main content of body-->
+		<div class="main_wrapper">
+		
+			<div class="side_bar">
+			
+			<div>
+			<div class="content_wrapper">
+			
+			</div>
+			
+		</div>
+		<!--end main content of body-->
+		
+		<!--Adding footer-->
+		<footer class="container-fluid text-center">
+			<a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+			<span style="color:white;" class="glyphicon glyphicon-chevron-up"></span>
+			</a><br><br>
+			<p>Designed By <a href="http://www.facebook.com/jannat2rahul" data-toggle="tooltip" title="Visit FB Profile">.......</a></p>
+		</footer>
+		<!--end of footer-->
+		
+		<!--adding bootstrap javascript file-->
+		<script src="js/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		
+		<!--adding defined javascript file-->
+		<script src="js/effects.js"></script>
+	</body>
+</html>
